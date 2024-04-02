1adf59688076a1b137cfe92c27d4cb6c0eb29961
viktorialoza <loza.viktoria2511@gmail.com>
2024-04-02 21:20:52 +0300

    Initial commit

---
+++ src/fourth.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class fourth {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введіть перше ім'я:");
+        String firstname = scanner.nextLine();
+
+        System.out.println("Введіть друге ім'я:");
+        String secondname = scanner.nextLine();
+
+        if (firstname.equalsIgnoreCase(secondname)) {
+            System.out.println("Імена ідентичні.");
+        } else {
+            System.out.println("Імена не ідентичні.");
+        }
+
+        scanner.close();
+    }
+}
